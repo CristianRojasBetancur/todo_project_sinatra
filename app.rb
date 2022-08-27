17d1b557c16130786f8776bd30bd3faaa9188681
CristianRojasBetancur <66922367+CristianRojasBetancur@users.noreply.github.com>
2022-08-27 00:04:26 -0500

    index filter

--- app.rb
+++ app.rb
@@ -4,10 +4,15 @@ require 'make_todo'
 
 # tasks = Tarea.all
 tasks = []
+complete_tasks = []
+incomplete_tasks = []
 
 get '/' do
 	@tasks = tasks
-
+	@type_tasks = params[:type_tasks]
+	@complete_tasks = complete_tasks
+	@incomplete_tasks = incomplete_tasks
+	
 	erb :index
 end
 
@@ -16,11 +21,15 @@ get '/new-task' do
 end
 
 post '/add-task' do
-	new_task = {}
-	new_task[:id] = Time.now.to_i
-	new_task[:name] = params[:name]
-	params[:status].eql?('true') ? new_task[:status] = true : new_task[:status] = false
+	params[:status].eql?('true') ? params[:status] = true : params[:status] = false
+	new_task = {
+		id: Time.now.to_i,
+		name: params[:name],
+		status: params[:status]
+	}
+
 	tasks << new_task
+	new_task[:status] ? complete_tasks << new_task : incomplete_tasks << new_task
 
 	redirect '/'
 end
@@ -32,8 +41,15 @@ post '/update-task/' do
 
 	tasks.each do |task|
 		if task[:id].eql?(id)
-			puts task
 			task[:status] = true unless task[:status]
+		end
+	end
+
+	incomplete_tasks.each do |task|
+		if task[:id].eql?(id)
+			index = incomplete_tasks.index(task)
+			incomplete_tasks.delete_at(index) if task[:status]
+			complete_tasks << task
 		end
 	end
 
@@ -45,10 +61,30 @@ end
 post '/delete-task/' do
 	id = params[:id].to_i
 
+	# all tasks array
+
 	tasks.each do |task|
 		if task[:id].eql?(id)
 			index = tasks.index(task)
 			tasks.delete_at(index)
+		end
+	end
+
+	# incomplete tasks array
+
+	incomplete_tasks.each do |task|
+		if task[:id].eql?(id)
+			index = incomplete_tasks.index(task)
+			incomplete_tasks.delete_at(index)
+		end
+	end
+
+	# complete tasks array
+
+	complete_tasks.each do |task|
+		if task[:id].eql?(id)
+			index = complete_tasks.index(task)
+			complete_tasks.delete_at(index)
 		end
 	end
 
